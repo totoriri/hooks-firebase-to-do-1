09997058c0c924c55da72e8bec6552ad7099ff1e
Risa Takata <beat1422@icloud.com>
2020-02-22 16:20:22 -0800

    set firebase to store the data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react'; // 修正
 import Todo from './pages/Todo';
-import styled from 'styled-components'
+import styled from 'styled-components';
+import firebase from 'firebase'; // 追記
+import 'firebase/firestore'; // 追記
 
 function App() {
 
@@ -8,28 +10,89 @@ function App() {
   const [input, setInput] = useState('');
   const [todoList, setTodoList] = useState([]);
   const [finishedList, setFinishedList] = useState([]);
+  // Loadingを判定する変数
+  const [isLoading, setIsLoading] = useState(true);
+  // 未完了のTodoが変化したかを監視する変数
+  const [isChangedTodo, setIsChangedTodo] = useState(false);
+  // 完了済みのTodoが変化したかを監視する変数
+  const [isChangedFinished, setIsChangedFinished] = useState(false);
 
-  const addTodo = () => {
+  const db = firebase.firestore(); // 追記
+
+  // 追記 一番最初にfirestoreからデータを取ってきてstateに入れる
+  useEffect(() => {
+    (async () => {
+      const resTodo = await db.collection("todoList").doc("todo").get();
+      // stateに入れる
+      setTodoList(resTodo.data().tasks);
+      const resFinishedTodo = await db.collection("todoList").doc("finishedtodo").get();
+      // stateに入れる
+      setFinishedList(resFinishedTodo.data().tasks);
+      // Loading終了
+      setIsLoading(false);
+    })()
+  }, [db])
+
+  useEffect(() => {
+    if (isChangedTodo) {
+      (async () => {
+        // 通信をするからLoadingをtrue
+        setIsLoading(true)
+        const docRef = await db.collection('todoList').doc('todo');
+        docRef.update({ tasks: todoList })
+        // Loading終了
+        setIsLoading(false)
+      })()
+    }
+  }, [todoList, isChangedTodo, db])
+
+  useEffect(() => {
+    if (isChangedFinished) {
+      (async () => {
+        // 通信をするからLoadingをtrue
+        setIsLoading(true)
+        const docRef = await db.collection('todoList').doc('finishedtodo');
+        docRef.update({ tasks: finishedList })
+        // Loading終了
+        setIsLoading(false)
+      })()
+    }
+    setIsChangedFinished(false)
+  }, [db, finishedList, isChangedFinished])
+
+  const addTodo = async () => {
     if (!!input) {
+      // 追記 Todoが変化したのでtrue
+      setIsChangedTodo(true);
       setTodoList([...todoList, input]);
       setInput('');
     }
   }
 
   const deleteTodo = (index) => {
+    // 追記 Todoが変化したからtrue
+    setIsChangedTodo(true);
     setTodoList(todoList.filter((_, idx) => idx !== index))
   }
 
   const deleteFinishTodo = (index) => {
+    // 追記 完了済みTodoが変化したからtrue
+    setIsChangedFinished(true);
     setFinishedList(finishedList.filter((_, idx) => idx !== index))
   }
 
   const finishTodo = (index) => {
+    // 追記 Todo、完了済みTodoがともに変化したからtrue
+    setIsChangedTodo(true);
+    setIsChangedFinished(true);
     deleteTodo(index)
     setFinishedList([...finishedList,todoList.find((_, idx) => idx === index)])
   }
 
   const reopenTodo = (index) => {
+    // 追記 Todo、完了済みTodoがともに変化したからtrue
+    setIsChangedTodo(true);
+    setIsChangedFinished(true);
     deleteFinishTodo(index)
     setTodoList([...todoList,finishedList.find((_, idx) => idx === index)])
   }
@@ -39,20 +102,24 @@ function App() {
       <Title>Todoリスト</Title>
       <input onChange={(e) => setInput(e.target.value)} value={input}/>
       <button onClick={() => addTodo()}>追加</button>
-      <TodoContainer>
-      {/* todoListという変数とdeleteTodoという関数をpropsとしてTodoコンポーネントに渡している*/}
-        <SubContainer>
-          <SubTitle>未完了</SubTitle>
-          <Todo todoList={todoList} deleteTodo={deleteTodo} changeTodoStatus={finishTodo} type="todo"/>
-        </SubContainer>
-        <SubContainer>
-          <SubTitle>完了済み</SubTitle>
-          <Todo todoList={finishedList} deleteTodo={deleteFinishTodo} changeTodoStatus={reopenTodo} type="done"/>
-        </SubContainer>
-      </TodoContainer>
+      {isLoading ? 
+        <Loading>loading</Loading>
+      :
+        <TodoContainer>
+        {/* todoListという変数とdeleteTodoという関数をpropsとしてTodoコンポーネントに渡している*/}
+          <SubContainer>
+            <SubTitle>未完了</SubTitle>
+            <Todo todoList={todoList} deleteTodo={deleteTodo} changeTodoStatus={finishTodo} type="todo"/>
+          </SubContainer>
+          <SubContainer>
+            <SubTitle>完了済み</SubTitle>
+            <Todo todoList={finishedList} deleteTodo={deleteFinishTodo} changeTodoStatus={reopenTodo} type="done"/>
+          </SubContainer>
+        </TodoContainer>
+      }
     </div>
   );
-}
+};
 
 export default App;
 
@@ -78,4 +145,8 @@ const TodoContainer = styled.div`
   width: 80%;
   margin: 0 auto;
   justify-content: space-between;
+`;
+
+const Loading = styled.div`
+  margin: 40px auto;
 `;
