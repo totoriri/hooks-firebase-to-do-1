9b0f4bf412d85218ef8de8a734b9d33fe7356475
Risa Takata <beat1422@icloud.com>
2020-02-24 23:46:29 -0800

    added some code to hide firebase config

---
+++ src/plugin/firebase.js
@@ -0,0 +1,17 @@
+import config from 'config'
+import firebase from 'firebase/app'
+
+const firebaseConfig = config.get('firebaseConfig')
+
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: firebaseConfig.apiKey,
+    authDomain: firebaseConfig.authDomain,
+    databaseURL: firebaseConfig.databaseURL,
+    projectId: firebaseConfig.projectId,
+    storageBucket: firebaseConfig.storageBucket,
+    messagingSenderId: firebaseConfig.messagingSenderId,
+    appId: firebaseConfig.appId
+  })
+}
+export default firebase
